d746921638fdd235af48e587a13f4049013b72bf
CX PC <CX PC@172.25.0.166>
2020-10-16 19:11:56 -0300

    clase1
    variables y vectores

--- app-domain/src/main/java/ar/com/educacionit/app/domain/App.java
+++ app-domain/src/main/java/ar/com/educacionit/app/domain/App.java
@@ -8,6 +8,38 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	//ctr+shift+x/y
+        //tipos datos PRIMITIVOS
+    	
+    	//byte < short < int < long < float < double
+    	
+    	//enteros sin decimales
+    	byte b = 127;
+    	short a = 125;
+    	    	
+    	int c = a;
+    	long d = 101;
+    	
+    	//------ con decimales
+    	float f = 151.8f;
+    	double dd = 150.5d;
+    	
+    	//V/F si/no
+    	boolean bool = false;
+    	
+    	//caracteres
+    	char cha = 'H';
+    	
+    	//WRAPPER
+    	String nombre = "hola";
+    	
+    	Byte by;
+    	Short sh;
+    	Integer edad;
+    	Long lo;
+    	Float flo;
+    	Double dob;
+    	Boolean alto;
+    	Character chara;
     }
 }
